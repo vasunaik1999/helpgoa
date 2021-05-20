7ffe45225fa70d00d48992683be0aa3b773e9a25
Vasu Naik <vasunaik1999@gmail.com>
2021-05-20 16:02:56 +0530

    added auth

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if (Auth::user()->hasRole('user')) {
+            return view('dashboard.userdashboard');
+        } else if (Auth::user()->hasRole('warrior')) {
+            return view('dashboard.warriordashboard');
+        } else if (Auth::user()->hasRole('admin')) {
+            return view('dashboard.admindashboard');
+        } else if (Auth::user()->hasRole('superadmin')) {
+            return view('');
+        }
+    }
+
+    public function myprofile()
+    {
+        return view('layouts.myprofile');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Auth Route for both
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+});
+
+
+//for users
+Route::group(['middleware' => ['auth', 'role:user']], function () {
+    Route::get('/dashboard/myprofile', 'App\Http\Controllers\DashboardController@myprofile')->name('dashboard.myprofile');
+});
+require __DIR__ . '/auth.php';
