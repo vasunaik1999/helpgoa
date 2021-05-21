93526f5ab10a75430dbac7df5952541509ee17c5
Vasu Naik <vasunaik1999@gmail.com>
2021-05-21 09:47:51 +0530

    Incomplete Sidebar added

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -1,144 +1,190 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
 
 <head>
-    <meta charset="utf-8">
+    <title>Bootstrap 4 Sidebar Menu Responsive with Sub menu Create Responsive Side Navigation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
     <style>
-        @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500');
-
-        * {
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            text-decoration: none;
-        }
-
         body {
-            font-family: 'Roboto', sans-serif;
+            background: #599fd9;
+            background-image: linear-gradient(135.9deg, rgba(109, 25, 252, 1) 16.4%, rgba(125, 31, 165, 1) 56.1%);
+            min-height: 100vh;
+            overflow-x: hidden;
         }
 
-        .sidebar {
-            position: fixed;
-            left: -250px;
-            width: 250px;
-            height: 100%;
-            background: #042331;
-            transition: all .5s ease;
-        }
-
-        .sidebar header {
-            font-size: 22px;
-            color: white;
-            line-height: 70px;
-            text-align: center;
-            background: #063146;
-            user-select: none;
-        }
-
-        .sidebar ul a {
-            display: block;
-            height: 100%;
-            width: 100%;
-            line-height: 65px;
-            font-size: 20px;
-            color: white;
-            padding-left: 40px;
-            box-sizing: border-box;
-            border-bottom: 1px solid black;
-            border-top: 1px solid rgba(255, 255, 255, .1);
-            transition: .4s;
-        }
-
-        ul li:hover a {
-            padding-left: 50px;
-        }
-
-        .sidebar ul a i {
-            margin-right: 16px;
-        }
-
-        #check {
-            display: none;
-        }
-
-        label #btn,
-        label #cancel {
-            position: absolute;
-            background: #042331;
-            border-radius: 3px;
-            cursor: pointer;
-        }
-
-        label #btn {
-            left: 40px;
-            top: 25px;
-            font-size: 35px;
-            color: white;
-            padding: 6px 12px;
-            transition: all .5s;
-        }
-
-        label #cancel {
-            z-index: 1111;
-            left: -195px;
-            top: 17px;
-            font-size: 30px;
-            color: #0a5275;
-            padding: 4px 9px;
-            transition: all .5s ease;
-        }
-
-        #check:checked~.sidebar {
-            left: 0;
-        }
-
-        #check:checked~label #btn {
-            left: 250px;
-            opacity: 0;
-            pointer-events: none;
-        }
-
-        #check:checked~label #cancel {
-            left: 195px;
-        }
-
-        #check:checked~section {
-            margin-left: 250px;
-        }
-
-        section {
-            background: url(bg.jpeg) no-repeat;
-            background-position: center;
-            background-size: cover;
+        .vertical-nav {
+            min-width: 17rem;
+            width: 17rem;
             height: 100vh;
-            transition: all .5s;
+            position: fixed;
+            top: 0;
+            left: 0;
+            box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+            transition: all 0.4s;
+        }
+
+        .page-content {
+            width: calc(100% - 17rem);
+            margin-left: 17rem;
+            transition: all 0.4s;
+        }
+
+        #sidebar.active {
+            margin-left: -17rem;
+        }
+
+        #content.active {
+            width: 100%;
+            margin: 0;
+        }
+
+        .separator {
+            margin: 3rem 0;
+            border-bottom: 1px dashed #fff;
+        }
+
+        .text-uppercase {
+            letter-spacing: 0.1em;
+        }
+
+        .text-gray {
+            color: #aaa;
+        }
+
+        .nav-link {
+            text-transform: capitalize;
+        }
+
+        .nav-link:hover {
+            background: #f3f2f2;
+        }
+
+        .text-primary {
+            color: #7579e7 !important;
+        }
+
+        @media (max-width: 768px) {
+            #sidebar {
+                margin-left: -17rem;
+            }
+
+            #sidebar.active {
+                margin-left: 0;
+            }
+
+            #content {
+                width: 100%;
+                margin: 0;
+            }
+
+            #content.active {
+                margin-left: 17rem;
+                width: calc(100% - 17rem);
+            }
         }
     </style>
 
 </head>
 
 <body>
-    <input type="checkbox" id="check">
-    <label for="check">
-        <i class="fas fa-bars" id="btn"></i>
-        <i class="fas fa-times" id="cancel"></i>
-    </label>
-    <div class="sidebar">
-        <header>My App</header>
-        <ul>
-            <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
-            <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
-            <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
-            <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
-            <li><a href="#"><i class="far fa-question-circle"></i>About</a></li>
-            <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
-            <li><a href="#"><i class="far fa-envelope"></i>Contact</a></li>
+
+    <!-- Vertical navbar -->
+    <div class="vertical-nav bg-white" id="sidebar">
+        <div class="py-4 px-3 mb-4 bg-light">
+            <div class="media d-flex align-items-center">
+                <img loading="lazy" src="images/p-1.png" alt="..." width="80" height="80" class="mr-3 rounded-circle img-thumbnail shadow-sm">
+                <div class="media-body">
+                    <h4 class="m-0">Olivia</h4>
+                    <p class="font-weight-normal text-muted mb-0">Web developer</p>
+                </div>
+            </div>
+        </div>
+
+        <p class="text-gray font-weight-bold text-uppercase px-3 small pb-4 mb-0">Dashboard</p>
+
+        <ul class="nav flex-column bg-white mb-0">
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark bg-light">
+                    <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+                    home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-address-card mr-3 text-primary fa-fw"></i>
+                    about
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                    services
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-picture-o mr-3 text-primary fa-fw"></i>
+                    Gallery
+                </a>
+            </li>
+        </ul>
+
+        <p class="text-gray font-weight-bold text-uppercase px-3 small py-4 mb-0">Charts</p>
+
+        <ul class="nav flex-column bg-white mb-0">
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-area-chart mr-3 text-primary fa-fw"></i>
+                    area charts
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-bar-chart mr-3 text-primary fa-fw"></i>
+                    bar charts
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-pie-chart mr-3 text-primary fa-fw"></i>
+                    pie charts
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark">
+                    <i class="fa fa-line-chart mr-3 text-primary fa-fw"></i>
+                    line charts
+                </a>
+            </li>
         </ul>
     </div>
-    <section></section>
+    <!-- End vertical navbar -->
+
+
+    <!-- Page content holder -->
+    <div class="page-content p-5" id="content">
+        <!-- Toggle button -->
+        <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4"><i class="fa fa-bars mr-2"></i><small class="text-uppercase font-weight-bold">Toggle</small></button>
+
+    </div>
+    <!-- End demo content -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+
+    <script>
+        $(function() {
+            $('#sidebarCollapse').on('click', function() {
+                $('#sidebar, #content').toggleClass('active');
+            });
+        });
+    </script>
 </body>
 
 </html>
