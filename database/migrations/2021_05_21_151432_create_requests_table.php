5dbd158f12bcf16a0b2a6138d8441d75bbf1f4ef
Vasu Naik <vasunaik1999@gmail.com>
2021-05-21 20:51:02 +0530

    created migration and controller for Request

---
+++ database/migrations/2021_05_21_151432_create_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('taluka')->nullable();
+            $table->string('city')->nullable();
+            $table->string('village')->nullable();
+            $table->string('landmark')->nullable();
+            $table->string('pincode')->nullable();
+            $table->dateTime('needed_by');
+            $table->integer('approaches');
+            $table->string('status');
+            $table->string('urgency_status');
+            $table->json('items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
