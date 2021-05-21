739333dca009ce27b29eef185d00c9ff9014bbaf
Vasu Naik <vasunaik1999@gmail.com>
2021-05-21 06:08:31 +0530

    Incomplete Sidebar 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Styles -->
@@ -33,7 +34,7 @@
 
         <!-- Page Content -->
         <main>
-            @include('layouts.sidebar')
+            @include('layouts.side')
 
             {{ $slot }}
 
