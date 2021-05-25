a83370aac9ba3852828de0767b3009a87f2baf3b
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-25 16:18:08 +0530

    updated dashboard views with card x-slot

--- resources/views/dashboard/superadmindashboard.blade.php
+++ resources/views/dashboard/superadmindashboard.blade.php
@@ -5,13 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in as Super Admin!
-                </div>
-            </div>
+    <x-slot name="card">
+        <div class="d-flex justify-content-center py-20">
+        You're logged in as Super Admin!
         </div>
-    </div>
+    </x-slot>
 </x-app-layout>
