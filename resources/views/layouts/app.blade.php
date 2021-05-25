91556d947329ecc5da815dd93f962256f54f13f4
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-25 14:36:38 +0530

    updated request views with card x-slot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
             <div class="py-6">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="card" style="height: 70vh; overflow-y:auto;">
+                        <div class="card shadow" style="height: 70vh; overflow-y:auto;">
                         {{ $card }}
                         </div>
                     </div>
--- resources/views/request/managerequests.blade.php
+++ resources/views/request/managerequests.blade.php
@@ -5,64 +5,58 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="table" class="table table-striped hover">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Phone</th>
-                                        <th>Address</th>
-                                        <th>Items</th>
-                                        <th>Urgncy Status</th>
-                                        <th>Status</th>
-                                        <th>Special Instructions</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($reqs as $req)
-                                    <tr>
-                                        <td>{{$req->id}}</td>
-                                        <td>{{$req->name}}</td>
-                                        <td>{{$req->phone}}</td>
-                                        <td>
-                                            {{$req->city}}, {{$req->taluka}}, {{$req->pincode}}
-                                            <br>{{$req->address}}
-                                        </td>
-                                        <td>
-                                            @foreach( json_decode($req->items) as $item)
-                                            {{$item}} <br>
-                                            @endforeach
-                                        </td>
-                                        <td>
-                                            {{$req->urgency_status}} <br>
-                                            Needed By:- {{$req->needed_by}}
-                                        </td>
-                                        <td>
-                                            <span class="badge badge-primary">{{$req->reqStatus}}</span>
-                                        </td>
-                                        <td>
-                                            {{$req->special_instructions}}
-                                        </td>
-                                        <td>
-                                            <a href="" class="btn btn-primary btn-sm">Edit</a>
-                                            <a href="{{url('/dashboard/'.$req->id.'/delete')}}" class="btn btn-danger btn-sm">Delete</a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
+    <x-slot name="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="table" class="table table-striped hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Items</th>
+                            <th>Urgncy Status</th>
+                            <th>Status</th>
+                            <th>Special Instructions</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($reqs as $req)
+                        <tr>
+                            <td>{{$req->id}}</td>
+                            <td>{{$req->name}}</td>
+                            <td>{{$req->phone}}</td>
+                            <td>
+                                {{$req->city}}, {{$req->taluka}}, {{$req->pincode}}
+                                <br>{{$req->address}}
+                            </td>
+                            <td>
+                                @foreach( json_decode($req->items) as $item)
+                                {{$item}} <br>
+                                @endforeach
+                            </td>
+                            <td>
+                                {{$req->urgency_status}} <br>
+                                Needed By:- {{$req->needed_by}}
+                            </td>
+                            <td>
+                                <span class="badge badge-primary">{{$req->reqStatus}}</span>
+                            </td>
+                            <td>
+                                {{$req->special_instructions}}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="{{url('/dashboard/'.$req->id.'/delete')}}" class="btn btn-danger btn-sm">Delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
 
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </div>
+    </x-slot>
 </x-app-layout>
