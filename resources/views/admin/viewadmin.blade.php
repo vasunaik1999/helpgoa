bab5321c15d59d59a9980740a0db2e70b5d7cc21
Vasu Naik <vasunaik1999@gmail.com>
2021-05-29 19:33:06 +0530

    bug fixes

--- resources/views/admin/viewadmin.blade.php
+++ resources/views/admin/viewadmin.blade.php
@@ -7,40 +7,42 @@
 
     <x-slot name="card">
         <div class="card-body">
-            <table id="table" class="table table-striped">
-                <thead>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Contact</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </thead>
-                <tbody>
-                    @foreach($admins as $key => $admin)
-                    <tr>
-                        <td>{{$key+1}}</td>
-                        <td>{{$admin->name}}</td>
-                        <td>
-                            {{$admin->phone}}
-                            @if($admin->secondaryPhone)<br>
-                            {{$admin->secondaryPhone}}
-                            @endif<br>
-                            {{$admin->email}}
-                        </td>
-                        <td>
-                            @if($admin->isBanned == 0)
-                            <span class="badge badge-success p-2">Not Banned</span>
-                            @else
-                            <span class="badge badge-danger p-2">Banned</span>
-                            @endif
-                        </td>
-                        <td>
-                            <a href="{{url('dashboard/view-admin/'.$admin->id.'/more-details')}}" class="btn btn-primary btn-sm">More Details</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table id="table" class="table table-striped">
+                    <thead>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Contact</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach($admins as $key => $admin)
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td>{{$admin->name}}</td>
+                            <td>
+                                {{$admin->phone}}
+                                @if($admin->secondaryPhone)<br>
+                                {{$admin->secondaryPhone}}
+                                @endif<br>
+                                {{$admin->email}}
+                            </td>
+                            <td>
+                                @if($admin->isBanned == 0)
+                                <span class="badge badge-success p-2">Not Banned</span>
+                                @else
+                                <span class="badge badge-danger p-2">Banned</span>
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{url('dashboard/view-admin/'.$admin->id.'/more-details')}}" class="btn btn-primary btn-sm">More Details</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </x-slot>
 
--- resources/views/user/viewusers.blade.php
+++ resources/views/user/viewusers.blade.php
@@ -7,41 +7,43 @@
 
     <x-slot name="card">
         <div class="card-body">
-            <table id="table" class="table table-striped">
-                <thead>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Contact</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </thead>
-                <tbody>
-                    @foreach($users as $key => $user)
-                    <tr>
-                        <td>{{$key+1}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>
-                            {{$user->phone}}
-                            @if($user->secondaryPhone)
-                            <br>
-                            {{$user->secondaryPhone}}
-                            @endif <br>
-                            {{$user->email}}
-                        </td>
-                        <td>
-                            @if($user->isBanned == 0)
-                            <span class="badge badge-success p-2">Not Banned</span>
-                            @else
-                            <span class="badge badge-danger p-2">Banned</span>
-                            @endif
-                        </td>
-                        <td>
-                            <a href="{{url('dashboard/view-user/'.$user->id.'/more-details')}}" class="btn btn-primary btn-sm">More Details</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table id="table" class="table table-striped">
+                    <thead>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Contact</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $key => $user)
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>
+                                {{$user->phone}}
+                                @if($user->secondaryPhone)
+                                <br>
+                                {{$user->secondaryPhone}}
+                                @endif <br>
+                                {{$user->email}}
+                            </td>
+                            <td>
+                                @if($user->isBanned == 0)
+                                <span class="badge badge-success p-2">Not Banned</span>
+                                @else
+                                <span class="badge badge-danger p-2">Banned</span>
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{url('dashboard/view-user/'.$user->id.'/more-details')}}" class="btn btn-primary btn-sm">More Details</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </x-slot>
 </x-app-layout>
