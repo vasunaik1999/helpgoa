bfebe66d43645960446fc56249f71c50b68305bb
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-30 00:42:38 +0530

    resourceDisinfectServices mcr created

--- app/Models/ResourceMedicineSuppliers.php
+++ app/Models/ResourceMedicineSuppliers.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ResourceMedicineSuppliers extends Model
 {
     use HasFactory;
-    use HasFactory;
     use SoftDeletes;
     protected $table = 'resource_oxygen_suppliers';
     protected $fillable = ['provider','contact','supplier_location','delivery_status','note','verified','visibility'];
