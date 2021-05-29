17de794561341986befd978c0375a43c62301d4f
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-29 18:58:25 +0530

    ResourceOxygenSupplier mcr created

---
+++ app/Http/Controllers/ResourceOxygenSuppliersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResourceOxygenSuppliers;
+use Illuminate\Http\Request;
+
+class ResourceOxygenSuppliersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResourceOxygenSuppliers  $resourceOxygenSuppliers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResourceOxygenSuppliers $resourceOxygenSuppliers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ResourceOxygenSuppliers  $resourceOxygenSuppliers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ResourceOxygenSuppliers $resourceOxygenSuppliers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResourceOxygenSuppliers  $resourceOxygenSuppliers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResourceOxygenSuppliers $resourceOxygenSuppliers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResourceOxygenSuppliers  $resourceOxygenSuppliers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResourceOxygenSuppliers $resourceOxygenSuppliers)
+    {
+        //
+    }
+}
---
+++ app/Models/ResourceOxygenSuppliers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResourceOxygenSuppliers extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'resource_oxygen_suppliers';
+    protected $fillable = ['provider','contact','supply_type','service_location','supplier_address','note','verified'];
+}
