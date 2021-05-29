d16ca6f78f733edbe4efd472b31611431b23c588
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-29 18:12:45 +0530

    ResourceDoctors mcr created

---
+++ app/Http/Controllers/ResourceDoctorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResourceDoctor;
+use Illuminate\Http\Request;
+
+class ResourceDoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResourceDoctor  $resourceDoctor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResourceDoctor $resourceDoctor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ResourceDoctor  $resourceDoctor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ResourceDoctor $resourceDoctor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResourceDoctor  $resourceDoctor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResourceDoctor $resourceDoctor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResourceDoctor  $resourceDoctor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResourceDoctor $resourceDoctor)
+    {
+        //
+    }
+}
---
+++ app/Models/ResourceDoctor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResourceDoctor extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'resource_doctors';
+    protected $fillable = ['name','consultation_type','availability','contact','location','note','verified'];
+}
