6d85ed2808e20e946f7a96115aa576c47bab232d
Rushabh Wadkar <rushabh171105066@gmail.com>
2021-05-29 18:45:13 +0530

    ResourceAmbulances mcr created

---
+++ app/Models/ResourceAmbulances.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResourceAmbulances extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'resource_ambulances';
+    protected $fillable = ['provider','ambulance_type','service_location','contact','note','verified'];
+}
