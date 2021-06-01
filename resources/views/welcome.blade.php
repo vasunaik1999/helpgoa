8fe55d730b4a251172bcc8e2ddd37cdb8a368967
Vasu Naik <vasunaik1999@gmail.com>
2021-06-01 13:25:39 +0530

    made table responsive for mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
 
   <!-- DataTable -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.24/af-2.3.6/b-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/fc-3.3.2/fh-3.1.8/kt-2.6.1/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sp-1.2.2/datatables.min.css" />
 
   <title>@yield('title')</title>
   <!-- Template Main CSS File -->
@@ -127,11 +127,30 @@
 
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <!-- DataTable -->
-  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.24/af-2.3.6/b-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/fc-3.3.2/fh-3.1.8/kt-2.6.1/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sp-1.2.2/datatables.min.js"></script>
+
+
+
 
   <script>
     $(document).ready(function() {
-      $('#table').DataTable();
+      $('#table').DataTable({
+        responsive: true,
+        autoWidth: false,
+        colReorder: true,
+        // rowReorder: true,
+
+
+        //BUTTONS TO DOWNLOAD
+        // dom: 'Bfrtip',
+        // buttons: [
+        //   'copy', 'csv', 'excel', 'pdf', 'print'
+        // ]
+
+      });
+
     });
   </script>
   @yield('script')
